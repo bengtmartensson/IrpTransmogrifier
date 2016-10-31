5614d80026d437607bf752db33d10284a0caaf6c
Bengt Martensson <barf@bengt-martensson.de>
2016-10-31 15:30:03 +0100

    Import fix

--- src/main/java/org/harctoolbox/irp/CodeGenerator.java
+++ src/main/java/org/harctoolbox/irp/CodeGenerator.java
@@ -19,7 +19,6 @@ package org.harctoolbox.irp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.logging.Logger;

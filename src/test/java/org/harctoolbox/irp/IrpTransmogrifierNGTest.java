f3f44a562f58f316415b8e152d907c73675cdbe7
Bengt Martensson <barf@bengt-martensson.de>
2016-12-25 20:14:45 +0100

    IrpTransmogrifierNGTest.java: Portability fix.

--- src/test/java/org/harctoolbox/irp/IrpTransmogrifierNGTest.java
+++ src/test/java/org/harctoolbox/irp/IrpTransmogrifierNGTest.java
@@ -1,6 +1,7 @@
 package org.harctoolbox.irp;
 
 import java.io.UnsupportedEncodingException;
+import org.harctoolbox.ircore.IrCoreUtils;
 import static org.testng.Assert.assertEquals;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -40,6 +41,6 @@ public class IrpTransmogrifierNGTest {
         System.out.println("analyze");
         String args = "-a 20 analyze -t 1p 0000 0073 0000 0012 000F 000A 0006 000A 0006 000A 0006 001B 0006 000A 0006 000A 0006 001B 0006 0015 0006 000A 0006 001B 0006 000A 0006 0015 0006 0010 0006 000A 0006 0015 0006 000A 0006 0015 0006 0C90";
         String result = IrpTransmogrifier.runMain(args);
-        assertEquals(result, "{36.0k,1p,msb}<6,-10|6,-16|6,-21|6,-27>(15,-10,A:32,6,-89m){A=0xc38c922}\tweight = 15\n");
+        assertEquals(result, "{36.0k,1p,msb}<6,-10|6,-16|6,-21|6,-27>(15,-10,A:32,6,-89m){A=0xc38c922}\tweight = 15" + IrCoreUtils.lineSeparator);
     }
 }

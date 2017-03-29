8931896010f9cd9d24e3797bc33618ad72432dcc
Bengt Martensson <barf@bengt-martensson.de>
2017-03-29 15:05:39 +0200

    Minor cleanup.

--- tools/paths.mk
+++ tools/paths.mk
@@ -7,7 +7,6 @@ IRPHOME := $(abspath $(MYDIR)../target)
 IRP_TRANSMOGRIFIER_JAR := $(IRPHOME)/IrpTransmogrifier-0.0.1dev-jar-with-dependencies.jar
 IRPPROTOCOLS_XML := $(IRPHOME)/IrpProtocols.xml 
 XSLT_DIR := $(IRPHOME)/xslt
-#XSLT_DIR := /home/bengt/harctoolbox/IrpTransmogrifier/src/main/xslt
 LIRC_TRANSFORM=$(XSLT_DIR)/lirc.xsl
 
 IRPTRANSMOGRIFIER := $(JAVA) -jar $(IRP_TRANSMOGRIFIER_JAR) -c $(IRPPROTOCOLS_XML) --loglevel warning --url-decode

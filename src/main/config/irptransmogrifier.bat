1a64cb33f161783aab1057702b2c1d10e9c4b026
Bengt Martensson <barf@bengt-martensson.de>
2018-11-02 12:30:39 +0100

    Spelling fix.

--- src/main/config/irptransmogrifier.bat
+++ src/main/config/irptransmogrifier.bat
@@ -5,7 +5,7 @@ REM Wrapper for IrpTransmogrifier as a command line program for Windows/MSDOS.
 REM The command line name to use to invoke java.exe, fix if desired.
 set JAVA=java
 
-REM Where the files are located, change of desired
+REM Where the files are located, change if desired
 set APPLICATIONHOME=%~dp0
 
 REM Normally no need to change the rest of the file

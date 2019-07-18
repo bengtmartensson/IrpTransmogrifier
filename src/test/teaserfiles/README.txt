9d35980ec0ecb50bd24cca820b990a8e6aea7cdd
Bengt Martensson <barf@bengt-martensson.de>
2019-07-18 12:10:35 +0200

    Update of API documentation

--- src/test/teaserfiles/README.txt
+++
@@ -1,4 +0,0 @@
-The data files here are from Teaser http://www.hifi-remote.com/forums/viewtopic.php?p=129017
-They are used with permission of the author.
-
-To execute as a test, execute the shell script dotest.sh.
--- tools/generate-irpprotocols_xml.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Generates IrpProtocols.xml from IrpProtocols.ini
-
-java -jar target/IrpTransmogrifier-0.0.1dev-jar-with-dependencies.jar -i src/main/config/IrpProtocols.ini -o src/main/config/IrpProtocols.xml  writeconfig

5ef3ef4fe849c8ee3036cf2672c4e3402117ef94
Bengt Martensson <barf@bengt-martensson.de>
2020-03-22 14:18:36 +0100

    Misc cleanup to test files.

--- contributed/input-filters/ict.sh
+++ contributed/input-filters/ict.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 JAVA=java
-#IRSCUTINIZER_JAR=/usr/local/share/irscrutinizer/IrScrutinizer-jar-with-dependencies.jar
-IRSCUTINIZER_JAR=/home/bengt/harctoolbox/harctoolboxbundle/target/IrScrutinizer-jar-with-dependencies.jar
+HERE="$(dirname $(dirname -- "$(readlink -f -- "${0}")" ) )"
+IRSCUTINIZER_JAR="${HERE}/../../IrScrutinizer/target/IrScrutinizer-jar-with-dependencies.jar"
 MAINCLASS=org.harctoolbox.irscrutinizer.importer.IctImporter
 
 exec "${JAVA}" -cp "${IRSCUTINIZER_JAR}" ${MAINCLASS} "$@"
